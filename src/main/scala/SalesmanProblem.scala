3048a92e0a969f14aa9498f0223aab8d382deb62
yassu <yassu0320.dev@gmail.com>
2018-11-29 01:41:25 +0900

    Update

--- src/main/scala/SalesmanProblem.scala
+++ src/main/scala/SalesmanProblem.scala
@@ -4,7 +4,7 @@ import scalafx.Includes._
 import scalafx.application.JFXApp
 import scalafx.application.JFXApp.PrimaryStage
 import scalafx.geometry.Insets
-import scalafx.scene.control.{MenuBar, Menu, MenuItem, RadioMenuItem, ToggleGroup}
+import scalafx.scene.control.{Spinner, Button, Label}
 import scalafx.scene.Scene
 import scalafx.scene.effect.DropShadow
 import scalafx.scene.layout.{HBox, VBox}
@@ -36,39 +36,34 @@ object ScalaFXHelloWorld extends JFXApp {
     )
   }
 
-  val menuBar = {
-    val _menuBar = new MenuBar
-
-    // ラジオボタン
-    val numberMenu = new Menu("Numbers Of Points")
-    val numberMenuItems =
-      Seq(10, 30, 50, 70, 100)
-      .map(n => new RadioMenuItem(n.toString))
-      .toList
-    val group = new ToggleGroup
-    group.toggles = numberMenuItems
-    numberMenu.items = numberMenuItems
-    // radioMenuが更新されたら gcを更新
-    numberMenu.onAction = (e: ActionEvent) => {
-      gc.clearRect(0, 0, CANVAS_SIZE, CANVAS_SIZE)
-      // TODO: 数値にできない場合どうするか考える
-      val n = numberMenuItems.filter(x => x.selected.value).head.text.value.toInt
-      points = (0 until n).map(n => (Random.nextInt(CANVAS_SIZE), Random.nextInt(CANVAS_SIZE)))
-        .toSeq
-      points.foreach(p => gc.fillOval(p._1, p._2, 10, 10))
+  val configBox = new HBox {
+    val numberOfSamplesLabel = new Label("Number Of Points:")
+    val numberOfSamplesSpinner = new Spinner[Int](10, 100, 30, 5)
+    numberOfSamplesSpinner.maxWidth = 100
+    val numberOfSampleButton = new Button {
+      text="Init"
+      onAction = () => {
+        gc.clearRect(0, 0, CANVAS_SIZE, CANVAS_SIZE)
+        // TODO: 数値にできない場合どうするか考える
+        val n = numberOfSamplesSpinner.value.value
+        points = (0 until n).map(n => (Random.nextInt(CANVAS_SIZE), Random.nextInt(CANVAS_SIZE)))
+          .toSeq
+        points.foreach(p => gc.fillOval(p._1, p._2, 10, 10))
+      }
     }
-
-    _menuBar.menus = List(numberMenu)
-    _menuBar
+    children = Seq(
+      numberOfSamplesSpinner,
+      numberOfSampleButton
+    )
   }
-
 
   stage = new PrimaryStage {
     title = "Canvas"
     scene = new Scene {
       content = new VBox {
         children=Seq(
-          menuBar,
+          configBox,
+          // menuBar,
           mainBox,
         )
       }
