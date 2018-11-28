6adbafd025bfbb41663938b9f8789cec9275b6fd
yassu <yassu0320.dev@gmail.com>
2018-11-29 01:03:18 +0900

    Update

--- src/main/scala/SalesmanProblem.scala
+++ src/main/scala/SalesmanProblem.scala
@@ -13,12 +13,14 @@ import scalafx.scene.paint.Color._
 import scalafx.scene.canvas._
 import scalafx.scene.paint.{Stops, LinearGradient}
 import scalafx.scene.text.Text
+import scala.util.Random
 import scalafx.scene.input.{KeyCodeCombination, KeyCode, KeyCombination}
 
 object ScalaFXHelloWorld extends JFXApp {
   private val CANVAS_SIZE = 600
   private val canvas = new Canvas(CANVAS_SIZE, CANVAS_SIZE)
   private val gc = canvas.graphicsContext2D
+  private var points = Seq[(Int, Int)]()
 
   val mainBox = new HBox {
     gc.strokePolyline(Seq(
@@ -38,9 +40,9 @@ object ScalaFXHelloWorld extends JFXApp {
     val _menuBar = new MenuBar
 
     // ラジオボタン
-    val numberMenu = new Menu("Numbers")
+    val numberMenu = new Menu("Numbers Of Points")
     val numberMenuItems =
-      Seq(100, 500, 1000, 1500, 2000)
+      Seq(10, 30, 50, 70, 100)
       .map(n => new RadioMenuItem(n.toString))
       .toList
     val group = new ToggleGroup
@@ -49,8 +51,11 @@ object ScalaFXHelloWorld extends JFXApp {
     // radioMenuが更新されたら gcを更新
     numberMenu.onAction = (e: ActionEvent) => {
       gc.clearRect(0, 0, CANVAS_SIZE, CANVAS_SIZE)
-      val s = numberMenuItems.filter(x => x.selected.value).head.text.value
-      gc.fillText(s, CANVAS_SIZE / 2, CANVAS_SIZE / 2)
+      // TODO: 数値にできない場合どうするか考える
+      val n = numberMenuItems.filter(x => x.selected.value).head.text.value.toInt
+      points = (0 until n).map(n => (Random.nextInt(CANVAS_SIZE), Random.nextInt(CANVAS_SIZE)))
+        .toSeq
+      points.foreach(p => gc.fillOval(p._1, p._2, 10, 10))
     }
 
     _menuBar.menus = List(numberMenu)
