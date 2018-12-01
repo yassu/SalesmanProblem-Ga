3094421965753401f97e5513158cd2ff3e84d31a
yassu <yassu0320.dev@gmail.com>
2018-12-01 22:08:39 +0900

    CrossOver

--- src/main/scala/logic/Individual.scala
+++ src/main/scala/logic/Individual.scala
@@ -1,3 +1,35 @@
 package salesman_problem.logic
+import scala.collection.mutable
 
-case class Individual[T](val rawItems: Seq[T])
+case class Individual[T](val rawItems: Seq[T]) {
+  def size: Int = rawItems.size
+  def apply(ind: Int): T = rawItems(ind)
+}
+
+object Individual {
+  private[logic] def crossOverOne[T](
+      parent1: Individual[T], parent2: Individual[T], fromLeft: Int, untilRight: Int):
+      Individual[T] = {
+      val len = parent1.size
+      var parentIndex = fromLeft
+      val rawChild = mutable.Map[Int, T]()
+
+      (fromLeft until len + fromLeft).foreach(_i => {
+        val i = _i % len
+        if (i >= fromLeft && i < untilRight) {
+          rawChild(i) = parent2.rawItems(i)
+        }
+        else {
+          while(rawChild.values.toSeq.contains(parent1(parentIndex))) {
+            parentIndex = (parentIndex + 1) % len
+          }
+          rawChild(i) = parent1(parentIndex)
+        }
+        parentIndex = (parentIndex + 1) % len
+      })
+
+      Individual(
+        for(i <- 0 until len) yield rawChild(i)
+      )
+    }
+}
--- src/test/scala/logic/IndividualSpec.scala
+++ src/test/scala/logic/IndividualSpec.scala
@@ -9,4 +9,19 @@ class IndividualSpec extends FunSpec {
       Individual[Int](Seq(1, 2, 3))
     }
   }
+
+  describe("About CrossOver") {
+    it("If parent1 == parent2 => child1 = parent1, child2 = parent2") {
+      val parent1 = Individual[Int](Seq(1, 2, 3, 4, 5, 6))
+      val parent2 = Individual[Int](Seq(1, 2, 3, 4, 5, 6))
+      val child = Individual.crossOverOne[Int](parent1, parent2, 2, 4)
+      assert(parent1 == child)
+    }
+    it("complex test") {
+      val parent1 = Individual[Int](Seq(2, 3, 1, 5, 4))
+      val parent2 = Individual[Int](Seq(2, 4, 3, 5, 1))
+      val child = Individual.crossOverOne[Int](parent1, parent2, 2, 4)
+      assert(child == Individual[Int](Seq(2, 1, 3, 5, 4)))
+    }
+  }
 }
