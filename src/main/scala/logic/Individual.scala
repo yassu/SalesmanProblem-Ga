acb43eb65af263327203c53c465dbcb8d4b68a00
yassu <yassu0320.dev@gmail.com>
2018-12-01 22:43:09 +0900

    Update some

--- src/main/scala/logic/Individual.scala
+++ src/main/scala/logic/Individual.scala
@@ -2,13 +2,13 @@ package salesman_problem.logic
 import scala.collection.mutable
 import scala.util.Random
 
-case class Individual[T](val rawItems: Seq[T]) {
+private[logic] case class Individual[T](val rawItems: Seq[T]) {
   def size: Int = rawItems.size
   def apply(ind: Int): T = rawItems(ind)
 }
 
-object Individual {
-  private[logic] def crossOverOne[T](
+private[logic] object Individual {
+  def crossOverOne[T](
       parent1: Individual[T], parent2: Individual[T], fromLeft: Int, untilRight: Int):
       Individual[T] = {
       val len = parent1.size
@@ -33,14 +33,16 @@ object Individual {
         for(i <- 0 until len) yield rawChild(i)
       )
     }
-  private[logic] def crossOver[T](
+
+  def crossOver[T](
       parent1: Individual[T], parent2: Individual[T], fromLeft: Int, untilRight: Int):
       (Individual[T], Individual[T]) =
       (
         crossOverOne[T](parent1, parent2, fromLeft, untilRight),
         crossOverOne[T](parent2, parent1, fromLeft, untilRight),
       )
-  private[logic] def crossOver[T](
+
+  def crossOver[T](
       parent1: Individual[T], parent2: Individual[T]):
       (Individual[T], Individual[T]) = {
     val len = parent1.size
