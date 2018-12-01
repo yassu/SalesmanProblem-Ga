789302361d89350a40f20da535b30acb72b129ae
yassu <yassu0320.dev@gmail.com>
2018-12-01 18:05:12 +0900

    eliteSelection

--- src/main/scala/logic/Population.scala
+++ src/main/scala/logic/Population.scala
@@ -4,6 +4,10 @@ import scala.util.Random
 
 case class Population[T](initIndividual: Individual[T]) {
   val size = 100
-  private val individuals = for(i <- (0 until size)) yield
+  val numberOfEliteSelection = 10
+
+  private var individuals = for(i <- (0 until size)) yield
     Individual(initIndividual.rawItems.sortBy(_ => Random.nextInt))
+
+  private def eliteSelection: Seq[Individual[T]] = individuals.take(numberOfEliteSelection)
 }
