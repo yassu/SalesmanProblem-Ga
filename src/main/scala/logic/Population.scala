d9205602a727d290fea0a816157838bb54f5f70d
yassu <yassu0320.dev@gmail.com>
2018-12-02 13:01:46 +0900

    mutation and crossOver

--- src/main/scala/logic/Population.scala
+++ src/main/scala/logic/Population.scala
@@ -4,13 +4,30 @@ import scala.util.Random
 
 case class Population[T](initIndividual: Individual[T], evalFunction: EvaluationFunction[T]) {
   val size = 100
-  val numberOfEliteSelection = 10
   val numberOfSelectedBySelection = 10
+  val numberOfEliteSelection = 10
+  val numberOfMutation = 10
+  val numberOfCrossingOver = (size - numberOfEliteSelection - numberOfMutation) / 2
 
   private var individuals = for(i <- (0 until size)) yield
     Individual(initIndividual.rawItems.sortBy(_ => Random.nextInt))
 
   private def eliteSelection: Seq[Individual[T]] = individuals.take(numberOfEliteSelection)
+  private def mutation: Seq[Individual[T]] = {
+    (0 until numberOfMutation).map(i => {
+      val parent = selectNicer
+      parent.mutation
+    })
+  }
+  private def crossOver: Seq[Individual[T]] =
+    (0 until numberOfCrossingOver)
+    .map(i => {
+      val parent1 = selectNicer
+      val parent2 = selectNicer
+      val (child1, child2) = Individual.crossOver(parent1, parent2)
+      Seq(child1, child2)
+    })
+    .flatMap(x => x)
 
   private def selectNicer: Individual[T] =
     Random.shuffle((0 until initIndividual.size).toList).take(10)
